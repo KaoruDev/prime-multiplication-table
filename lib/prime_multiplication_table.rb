5659823c545db11643fedf2f0a2583d2e09e3e5d
Kaoru Kohashigawa <dev@kaoruk.com>
2015-11-10 19:56:08 -0800

    git prime multiplication to return a matrix of four rows

--- lib/prime_multiplication_table.rb
+++ lib/prime_multiplication_table.rb
@@ -1,4 +1,24 @@
+require "matrix"
+
 class PrimeMultiplicationTable
-  def generate(prime_numbers)
+  def generate(prime_numbers=[])
+    matrix = Matrix[prime_numbers.unshift(0)]
+
+    prime_numbers[1..-1].inject(matrix) do |result, prime|
+      row = build_row(prime, prime_numbers[1..-1])
+      add_row_to_matrix(row, result)
+    end
   end
+
+  def build_row(prime, prime_numbers)
+    prime_numbers.inject([prime]) do |row, row_prime|
+      row << prime * row_prime
+      row
+    end
+  end
+
+  def add_row_to_matrix(row, matrix)
+    Matrix.rows(matrix.to_a << row)
+  end
+
 end
--- spec/prime_multiplication_table_spec.rb
+++ spec/prime_multiplication_table_spec.rb
@@ -11,8 +11,24 @@ RSpec.describe PrimeMultiplicationTable do
         PrimeMultiplicationTable.new.generate([1, 2, 3])
       }
 
+      it "that has 4 rows" do
+        expect(matrix.row_count).to eq(4)
+      end
+
       it "with first row [0, 1, 2, 3]" do
-        expect(matrix.row(0)).to eq([0, 1, 2, 3])
+        expect(matrix.row(0).to_a).to eq([0, 1, 2, 3])
+      end
+
+      it "with second row [1, 1, 2, 3]" do
+        expect(matrix.row(1).to_a).to eq([1, 1, 2, 3])
+      end
+
+      it "with third row [2, 2, 4, 6]" do
+        expect(matrix.row(2).to_a).to eq([2, 2, 4, 6])
+      end
+
+      it "with fourth row [2, 2, 4, 6]" do
+        expect(matrix.row(3).to_a).to eq([3, 3, 6, 9])
       end
     end
   end
