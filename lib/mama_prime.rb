44043f0836e51f0941f2f35d57cf7e4cd00bbc0e
Kaoru Kohashigawa <dev@kaoruk.com>
2015-11-10 17:50:39 -0800

    Passes simply array spec for MamaPrime#birth

---
+++ lib/mama_prime.rb
@@ -0,0 +1,5 @@
+class MamaPrime
+  def birth
+    []
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,3 +2,8 @@ $:.unshift File.expand_path(Dir.pwd, '..')
 
 Bundler.require(:default, :test)
 
+# add Games folder to ruby's load path
+Dir["#{File.expand_path(Dir.pwd, '..')}/lib/**/*.rb"].each do |file|
+  require_relative file
+end
+
